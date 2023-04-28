d72b0b72bd80fa3b40e29429944cc71172bccc9c
fathimasilviyak <silviyakvm@gmail.com>
2023-04-28 18:26:57 +0530

    make admin panel for offer

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Products
+from .models import Products, Offer
+
 
 class ProductsAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'stock')
 
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('code', 'description', 'discount')
+
 # Register your models here.
 admin.site.register(Products, ProductsAdmin)
+admin.site.register(Offer, OfferAdmin)
