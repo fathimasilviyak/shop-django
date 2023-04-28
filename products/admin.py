8206b81d3a1cd23b97491e818ebf6e5e243d7e8c
fathimasilviyak <silviyakvm@gmail.com>
2023-04-28 18:19:45 +0530

    make admin panel for products

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Products
+
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
 
 # Register your models here.
+admin.site.register(Products, ProductsAdmin)
