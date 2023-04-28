02feb91fecf4a0d5aac9d218517fc96adf35af73
fathimasilviyak <silviyakvm@gmail.com>
2023-04-28 21:34:36 +0530

    get data in the view

--- products/views.py
+++ products/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Products, Offer
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1>Hello.. Welcome to Django!</h1>")
+    offers = Offer.objects.all()
+    products = Products.objects.all()
+    return render(request, 'index.html',{'offers': offers, 'products': products})
+    # return HttpResponse("<h1>Hello.. Welcome to Django!</h1>")
 
 def about(request):
     return HttpResponse("<h1>About page</h1>")
